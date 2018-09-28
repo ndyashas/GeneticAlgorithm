f5d6d65883bff7cc6894a1b6797a67026b93341f
Yashas ND <yashasbharadwaj111@gmail.com>
2018-09-28 09:39:09 +0530

    Test commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ga2",
-    version="0.2.2",
+    version="0.2.3",
     author="Yashas ND",
     author_email="yashasbharadwaj111@gmail.com",
     description="A simple robust Genetic Algorithm utility",
