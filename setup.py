ed8a4b3c7d0ee08bc80a78ba69954d00434c2a74
Yashas ND <yashasbharadwaj111@gmail.com>
2018-09-28 08:59:58 +0530

    Added setup.py for PyPi

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ga2",
+    version="0.1.0",
+    author="Yashas ND",
+    author_email="yashasbharadwaj111@gmail.com",
+    description="A simple robust Genetic Algorithm utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yashasbharadwaj111/GeneticAlgorithm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Artificial Life",
+        "Intended Audience :: Science/Research",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
+        
