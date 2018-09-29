860c1d1635ff9e3ad9a725d4ea74d8671f5a8f11
Yashas ND <yashasbharadwaj111@gmail.com>
2018-09-29 17:21:05 +0530

    Staged setup.py for 0.2.6 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ga2",
-    version="0.2.5",
+    version="0.2.7",
     author="Yashas ND",
     author_email="yashasbharadwaj111@gmail.com",
     description="A simple robust Genetic Algorithm utility",
